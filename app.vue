47a3ddc28fb513c33495b2b9a145343f157c91f9
m4olivei <m4olivei@gmail.com>
2024-12-17 16:00:57 -0500

    Initial commit

---
+++ app.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import '@carbon/ibmdotcom-web-components/es/components/footer/indexj.js';
+</script>
+<template>
+  <div>
+    <c4d-footer-container></c4d-footer-container>
+  </div>
+</template>
